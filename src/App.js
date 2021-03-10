1db8bb518771639099fb0098e117573619a35067
Chris Dzoba <chris@dzoba.com>
2021-03-09 21:30:32 -0800

    Functioning chat app with Tezos chain

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, {useEffect, useState} from 'react';
-import randomWords from 'random-words';
 import { TezosToolkit } from '@taquito/taquito';
+import { importKey } from '@taquito/signer';
+
 import './App.css';
-const tezos = new TezosToolkit('https://api.tez.ie/rpc/delphinet');
+const faucet = require('./faucet.json')
+const tezos = new TezosToolkit('https://api.tez.ie/rpc/edonet');
+
+const contractAddress = `KT1QDM6Bv181yaZXhQcTjZRTK8UZTvfidjmU`
+
+importKey(
+  tezos,
+  faucet.email,
+  faucet.password,
+  faucet.mnemonic.join(' '),
+  faucet.secret
+).catch((e) => console.error(e));
 
 function App() {
   const [messages, setMessages] = useState([])
@@ -10,21 +22,31 @@ function App() {
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setMessages([...messages, {
-        text: randomWords({ min: 3, max: 10, join: ' ' }), 
-        author: `anonymous`
-      }])
-
+      tezos.contract
+        .at(contractAddress)
+        .then((contract) => {
+          contract.storage().then(res => {          
+            setMessages(res.messages)
+          })
+        })
+        .catch((error) => console.log(`Error: ${JSON.stringify(error, null, 2)}`));
     }, 5000)
 
     return () => clearInterval(intervalId);
   })
 
   const submit = () => {
-    setMessages([...messages, {
-      text: value, 
-      author: `you`
-    }])
+    tezos.contract
+    .at(contractAddress)
+    .then((contract) => {
+      return contract.methods.post(value).send();
+    })
+    .then((op) => {
+      console.log(`Awaiting for ${op.hash} to be confirmed...`);
+      return op.confirmation(3).then(() => op.hash);
+    })
+    .then((hash) => console.log(`Operation injected: https://edo.tzstats.com/${hash}`))
+    .catch((error) => console.log(`Error: ${JSON.stringify(error, null, 2)}`));
     setValue('')
   }
 
@@ -34,23 +56,20 @@ function App() {
     }
   }
 
-  const accessTezosNetwork = () => {
-    tezos.tz
-    .getBalance('tz1h3rQ8wBxFd8L9B3d7Jhaawu6Z568XU3xY')
-    .then((balance) => {
-      setMessages([...messages, {
-        text: `${balance.toNumber() / 1000000} ꜩ`, 
-        author: `Tezos Network`
-      }])
+  const rocket = () => {
+    tezos.contract
+    .at(contractAddress)
+    .then((contract) => {
+      return contract.methods.rocket([["1"]]).send();
     })
-    .catch((error) => console.log(JSON.stringify(error)));
+    .catch((error) => console.log(`Error: ${JSON.stringify(error, null, 2)}`));
   }
 
   return (
     <div className="App" style={{display: `flex`, flexDirection: `column`, width: `100%`, padding: `10px`, marginBottom: `50px`}}>
-      {messages.map((message, index) => {
+      {messages.map((message) => {
         return (
-        <div key={message.text} style={{
+        <div key={message.timestamp} style={{
           border: `1px solid lightgray`, 
           marginBottom: `10px`, 
           padding: `10px`, 
@@ -60,7 +79,7 @@ function App() {
           boxShadow: `2px 2px 9px -6px #000000`,
           borderRadius: `5px`,
         }}>
-          <span style={{color: `gray`, fontSize: 10}}>{message.author} said:</span>
+          <span style={{color: `gray`, fontSize: 10}}>{message.sender} said:</span>
           <span>{message.text}</span>
         </div>
 
@@ -70,7 +89,7 @@ function App() {
       <div style={{position: `fixed`, bottom: 0, left: 0, height: `50px`, width: `100%`, backgroundColor: `gray`}}>
         <input value={value} onKeyDown={handleKeyDown} onChange={e => setValue(e.target.value)} type="text" style={{marginLeft: `10px`, marginTop: `2px`, height: `40px`, width: `800px`}}></input>
         <button style={{marginLeft: `5px`, height: `40px`}} onClick={submit} >Send</button>
-        <button onClick={accessTezosNetwork}>Access Tezos Network </button>
+        <button onClick={rocket}>rocket!</button>
       </div>
     </div>
   );
